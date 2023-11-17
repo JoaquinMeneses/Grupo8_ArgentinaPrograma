b14995fd98b30b1998b1a96c03c040a4049b7f8d
Joaquin Meneses <joakinmeneses.jm@gmail.com>
2023-11-17 19:41:55 -0300

    Add DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Template from "./pages/Template";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <Template/>
+    <Main/>
   );
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Template = () => {
+  return (
+    <button className="btn btn-primary">Button</button>
+
+  );
+};
+
+export default Template;
